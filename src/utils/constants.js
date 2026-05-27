3f67339f342cbe08be9c60a8697c7185f1af9998
web-abhishek <web.abhiacharya@gmail.com>
2026-05-27 14:14:35 +0530

    Fix API proxy for Netlify

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,23 @@
+// export const CDN_URL =
+//   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+// export const REST_LIST = "https://corsproxy.io/https://namastedev.com/api/v1/listRestaurants";
+
+// export const REST_MENU_LIST = "https://corsproxy.io/https://namastedev.com/api/v1/listRestaurantMenu/";
+// export const REST_MENU_LIST_IMG = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+// export const FOOD_ITEM_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
+
+
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const REST_LIST = "https://corsproxy.io/https://namastedev.com/api/v1/listRestaurants";
+export const REST_LIST = "/api/listRestaurants";
 
-export const REST_MENU_LIST = "https://corsproxy.io/https://namastedev.com/api/v1/listRestaurantMenu/";
-export const REST_MENU_LIST_IMG = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const REST_MENU_LIST = "/api/listRestaurantMenu/";
 
-export const FOOD_ITEM_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
+export const REST_MENU_LIST_IMG =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const FOOD_ITEM_CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
